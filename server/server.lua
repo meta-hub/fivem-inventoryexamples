33d378fa17018009dad9ec39bc8f9cd2ba833e65
meta-hub <56391287+meta-hub@users.noreply.github.com>
2021-08-08 12:53:19 +0930

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -5,19 +5,30 @@ end)
 Citizen.CreateThread(function()
   exports["mf-inventory"]:onReady(function()
     for _,data in pairs(Config.Objects) do
-      if data.InvType == "vault" then
+      if data.InvType ~= "crafting" then
         local inv = exports["mf-inventory"]:getInventory(data.Identifier)
         if not inv then
           print(string.format("creating new vault inventory for identifier '%s'",data.Identifier))
 
-          exports["mf-inventory"]:createInventory(
-            data.Identifier,
-            "inventory",
-            "vault",
-            "vault_storage",
-            data.MaxWeight or 500.0,
-            data.MaxSlots or 50
-          )
+          if data.Temporary then
+            exports["mf-inventory"]:createTemporaryInventory(
+              data.Identifier,
+              "inventory",
+              data.InvType,
+              data.InvLabel,
+              data.MaxWeight or 500.0,
+              data.MaxSlots or 50
+            )
+          else
+            exports["mf-inventory"]:createInventory(
+              data.Identifier,
+              "inventory",
+              data.InvType,
+              data.InvLabel,
+              data.MaxWeight or 500.0,
+              data.MaxSlots or 50
+            )
+          end
         end
       end
     end
